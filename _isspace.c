b438657f4d081cb225faa3fd75dc5f854eb19bda
Mathenge-Alex <arxmaina@gmail.com>
2024-02-29 05:48:32 +0300

    feat: Check if space or double space

--- _isspace.c
+++ _isspace.c
@@ -1,9 +1,9 @@
 #include "ctype.h"
 
 /**
- * _isspace - check if a character is whitespace
+ * _isspace - function checks if a character is whitespace
  * @c: the character to check
- * Return: If c is whitespace, return 1. Otherwise, return 0.
+ * Return: If c is whitespace, return 1, or 0.
  */
 bool _isspace(int c)
 {
--- _isspecial_double.c
+++ _isspecial_double.c
@@ -1,9 +1,10 @@
 #include "quote.h"
 
 /**
- * _isspecial_double - check if a character is special inside double quotes
+ * _isspecial_double - function checks if a character is
+ * special inside double quotes
  * @c: the character to check
- * Return: If c is special, return 1. Otherwise, return 0.
+ * Return: If c is special, return 1, otherwise 0.
  */
 int _isspecial_double(char c)
 {

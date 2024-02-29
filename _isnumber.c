5d39ee69a48436501e1456e80177cd28326c4248
Mathenge-Alex <arxmaina@gmail.com>
2024-02-29 05:48:06 +0300

    feat: Check if number

--- _isnumber.c
+++ _isnumber.c
@@ -1,9 +1,9 @@
 #include "ctype.h"
 
 /**
-  * _isnumber - check if a string consists only of digits
+  * _isnumber - func checks if a string consists of only of digits
   * @s: pointer to string
-  * Return: true or false
+  * Return: returns true or false
   */
 bool _isnumber(const char *s)
 {

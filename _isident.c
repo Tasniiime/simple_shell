5979b4586f2246bf4b46c597e8ba176078548930
Mathenge-Alex <arxmaina@gmail.com>
2024-02-29 05:47:56 +0300

    feat: Check if identifier

--- _isident.c
+++ _isident.c
@@ -1,9 +1,9 @@
 #include "ctype.h"
 
 /**
-  * _isident - checks if the character is a valid identifier character
+  * _isident - function checks if the character is a valid identifier character
   * @c: character to check
- * Return: If c is an identifier character, return 1. Otherwise, return 0.
+  * Return: Returns 1 if c is alphanumeric, or 0
   */
 bool _isident(int c)
 {

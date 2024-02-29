cf0812cdac06c55d8d02c66e23332c8683e2cb47
Mathenge-Alex <arxmaina@gmail.com>
2024-02-29 05:48:14 +0300

    feat: Check if quote

--- _isquote.c
+++ _isquote.c
@@ -1,9 +1,9 @@
 #include "ctype.h"
 
 /**
- * _isquote - check if a character is a quote
+ * _isquote - func checks if a character is a quote
  * @c: the character to check
- * Return: If c is a quote, return 1. Otherwise, return 0.
+ * Return: If c is a quote, returns 1, or 0.
  */
 bool _isquote(int c)
 {
